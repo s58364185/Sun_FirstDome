bd1a250ed868dcc31b28dc03546de09f53228095
Sujun Sun <sujun.sun@yinyuetai.com>
2013-07-03 18:41:38 +0800

    test

---
+++ Sun_FirstDome/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sun_FirstDome
+//
+//  Created by shuilin on 13-7-2.
+//  Copyright (c) 2013年 YYT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
